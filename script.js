0f61e3c8fb1568289661ffbd07dfc289b3827757
Flaige <150943265+Flaige@users.noreply.github.com>
2024-06-16 12:35:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 let colorInterval = null;
 
 startButton.addEventListener('click', () => {
-    // Deaktywuj przycisk Start, aby nie można było go kliknąć ponownie
     startButton.disabled = true;
 
-    // Ustaw interwał zmiany koloru tła
     colorInterval = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
 });
 
 stopButton.addEventListener('click', () => {
-    // Zatrzymaj interwał zmiany koloru
     clearInterval(colorInterval);
 
-    // Aktywuj przycisk Start, aby można było go ponownie kliknąć
     startButton.disabled = false;
 });
-
-// Funkcja generująca losowy kolor HEX
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
